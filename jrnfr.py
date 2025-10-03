cacfa5c8b9e4bf9dcf9252c7a85575a200932017
aelric <180872863+aelric1@users.noreply.github.com>
2025-10-03 10:35:45 +0000

    Delete jrnfr.py

--- jrnfr.py
+++
@@ -1,7 +0,0 @@
-import random 
-import string
-t=int(input("enter the genratations time : "))
-s=int(input("enter the passwords chars : "))
-chars=''.join(string.digits +string.ascii_letters+ "!@#$%")
-for i in range(t):
-    print(''.join(random.choices(chars,k=s)))
